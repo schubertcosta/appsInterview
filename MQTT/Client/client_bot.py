651c8b709f63665d22dbddaf17dac256768150b5
Felipe Schubert Costa <schubert.cubo@gmail.com>
2023-03-22 19:23:26 +0100

    small fixes to implement mqtt communication

--- MQTT/Client/client_bot.py
+++ MQTT/Client/client_bot.py
@@ -3,14 +3,15 @@ import os
 
 # assigning clientId from docker to clientid
 client_id = os.environ["HOSTNAME"]
+bot_topic = os.environ["SUBTOPIC"]
 
 def on_connect(client, user_data, flags, rc):
     print(f"Bot Client: {client_id} connected to Mosquitto broker.")
-    client.subscribe("bot/topic")
+    client.subscribe(bot_topic)
 
 def on_message(client, user_data, msg):
-    print(msg.topic + " " + str(msg.payload))
-    client.publish("bot/topic/ack", f"Message Received by Bot Client {client_id}")
+    print(f"{client_id} | {msg.topic}: {msg.payload.decode()}")
+    client.publish(f"{bot_topic}/ack", f"Message Received by Bot Client {client_id}")
     
 def main():
     client = mqtt.Client(client_id)
