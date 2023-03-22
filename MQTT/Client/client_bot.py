39f343b40c502761d135ad2767e0b3302dd389b4
Felipe Schubert Costa <schubert.cubo@gmail.com>
2023-03-22 18:01:17 +0100

    adjusting folder location + adding full example working with network with docker

--- MQTT/Client/client_bot.py
+++ MQTT/Client/client_bot.py
@@ -1,23 +1,23 @@
-from time import sleep
 import paho.mqtt.client as mqtt
+import os
 
-client_id = "user_client"
+# assigning clientId from docker to clientid
+client_id = os.environ["HOSTNAME"]
 
 def on_connect(client, user_data, flags, rc):
-    print(f"Client: {client_id} connected to Mosquitto broker.", str(rc))
-    client.subscribe("bot/topic/ack")
-    client.publish("bot/topic", f"Wakeup Bots")
+    print(f"Bot Client: {client_id} connected to Mosquitto broker.")
+    client.subscribe("bot/topic")
 
 def on_message(client, user_data, msg):
     print(msg.topic + " " + str(msg.payload))
+    client.publish("bot/topic/ack", f"Message Received by Bot Client {client_id}")
     
 def main():
     client = mqtt.Client(client_id)
     client.on_connect = on_connect
     client.on_message = on_message
 
-    client.connect("host.docker.internal", 1883, 60)    
-    
+    client.connect("mosquitto-broker", 1883, 60)
     client.loop_forever()
     
 main()
